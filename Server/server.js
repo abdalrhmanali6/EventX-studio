c60cb5bea4d9d1a56d8a8ea07ac7d310a95c13f7
abdalrhmanali6 <abdalrahmanali632006@gmail.com>
2025-09-04 18:25:20 +0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ const { Parser } = require("json2csv");
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(console.log("connected"))
+  .then(()=>console.log("connected"))
   .catch((e) => console.error(e));
 
 app.use(express.json());
@@ -717,11 +717,9 @@ app.get(
   }
 );
 
-app.listen(5000, (err) => {
-  console.log("Server is open on http://localhost:5000");
-  if (err) {
-    console.log(err);
-  }
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 //?.......................................................Functions.........................................................................................
